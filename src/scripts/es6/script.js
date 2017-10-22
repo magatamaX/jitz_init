e0928e2e50b4a9b18817fe8d2f25bb3397309f38
magatamaX <hizakitomohiro.r@gmail.com>
2017-10-22 23:46:51 +0900

    スライダーの仕様変更つづきっ！

--- src/scripts/es6/script.js
+++ src/scripts/es6/script.js
@@ -8,15 +8,39 @@
   const absbox = elm5.appendChild(ul);
   absbox.appendChild(elm1Clone);
 
-  elm.addEventListener("transitionend", function(e){
-    console.log(e);
-    // alert('animation--End!');
-    // elm.classList.add('trs01');
-    elm.classList.add('sld02'); 
-  });
+  const slides = elm.children;
+  console.log(slides);
+  console.log(slides.length);
+
+let n = 0;
+
+elm.addEventListener("transitionend", function(e){
+
+
+  console.log('アニメーション終わりました');
+  console.log(n);
+  console.log(slides.length);
+
+
+
+  if ( n < slides.length-1 ){
+    n++;
+  } else {
+    elm.classList.remove('sld0');
+    elm.classList.remove('sld1');
+    elm.classList.remove('sld2');
+    elm.classList.remove('sld3');
+    elm.classList.remove('sld4');
+    n = 0;
+  }
+
+  elm.classList.add('sld'+n);
+
+
+});
 
   window.onload = function(){
-    elm.classList.add('sld01');
+    elm.classList.add('sld0');
   };
 
 
